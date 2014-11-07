a7b0398008d46f3dbb3dc603bb09c48ca6164e9f
Alix Fumoleau <alix.fumoleau@gmail.com>
2014-11-08 00:59:41 +0100

    Add some tests objects

---
+++ Assets/ColdGauge.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+public var ship : Transform;
+private var shipPosition : Vector3;
+
+function Start () {
+	shipPosition = ship.position;
+}
+
+function Update () {
+	var offset = ship.position - shipPosition;
+	//GetComponent(Slider).value = ;
+	shipPosition = ship.position;
+}
---
+++ Assets/ShipNavigation.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update ()
+{
+	if(Input.GetKey("up"))
+	{
+		//rigidbody.AddForce
+	}
+	if(Input.GetKey("left"))
+	{}
+
+	if(Input.GetKey("right"))
+	{}
+
+	if(Input.GetKey("down"))
+	{}
+
+}
