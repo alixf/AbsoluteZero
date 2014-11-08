f924cb645cca79e5c3c467967d2c371f528eb8de
krakenRose <orain.juliette@gmail.com>
2014-11-08 02:07:52 +0100

    Mise en place de la cold gauge

--- Assets/ColdGauge.js
+++ Assets/ColdGauge.js
@@ -3,14 +3,36 @@
 import UnityEngine.UI;
 
 public var ship : Transform;
-private var shipPosition : Vector3;
+public var factor : float;
+private var shipLastPosition : Vector3;
 
 function Start () {
-	shipPosition = ship.position;
+	shipLastPosition = ship.position;
 }
 
 function Update () {
-	var offset = ship.position - shipPosition;
-	//GetComponent(Slider).value = ;
-	shipPosition = ship.position;
+	var xa = ship.position.x;
+	var ya = ship.position.y;
+	var xb = shipLastPosition.x;
+	var yb = shipLastPosition.y;
+	
+	var distance = Mathf.Sqrt(((xb-xa)*(xb-xa))+((yb-ya)*(yb-ya)));
+	GetComponent(Slider).value -= distance*factor;
+	shipLastPosition = ship.position;
+	ship.position.x += 0.01;
+	
+	if(Input.GetKeyDown('w')){
+		warmUp(0.5);
+	}
+	if(GetComponent(Slider).value == 0){
+		Debug.Log("Frisson");
+		warmUp(1);
+	}
+	
 }
+
+function warmUp(degrees:float) {
+	GetComponent(Slider).value += degrees;
+}
+
+
