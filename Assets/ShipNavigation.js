72e3d91a5a609ec9e1b55f4107dcbaaa8fc97090
Alix Fumoleau <alix.fumoleau@gmail.com>
2014-11-08 04:00:04 +0100

    Add ship movements and Shaderforge

--- Assets/ShipNavigation.js
+++ Assets/ShipNavigation.js
@@ -1,22 +1,20 @@
 ﻿#pragma strict
 
+public var acceleration = 50;
+
 function Start () {
 
 }
 
-function Update ()
+function FixedUpdate ()
 {
-	if(Input.GetKey("up"))
-	{
-		//rigidbody.AddForce
-	}
-	if(Input.GetKey("left"))
-	{}
+}
 
-	if(Input.GetKey("right"))
-	{}
+function Update()
+{
+	var mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0));
+	transform.eulerAngles = new Vector3(0.0,0.0, 90 + Mathf.Rad2Deg * Mathf.Atan2(transform.position.y-mousePosition.y, transform.position.x-mousePosition.x));
 
-	if(Input.GetKey("down"))
-	{}
-
+	if(Input.GetMouseButton(1))
+		rigidbody2D.AddRelativeForce(acceleration * new Vector2(0,1));
 }
